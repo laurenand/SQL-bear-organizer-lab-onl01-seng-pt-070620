f29bc195743647e48d661869dad2877c3fb6924a
Lauren Anderson <laurenaanderson95@gmail.com>
2020-09-14 21:36:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,6 +1,6 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT(gender) FROM bears GROPUP BY gender = Female"
-  return "SELECT(name, age) "
+  "SELECT(gender) FROM bears GROUP BY gender = Female"
+  return "SELECT(name, age)"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
